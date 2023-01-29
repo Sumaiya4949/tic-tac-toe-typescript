a4e2b38b850dd6bf56bd2006c24c43a2c85c90ca
Sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2023-01-29 12:06:32 +0100

    Make the app interactive with player change and game board value

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import { ScoreBoard } from './ScoreBoard';
 import { TicTacToeGrid } from './TicTacToeGrid';
-import type { CellClickHandler, CellIndex, Grid9Values, WinnerCellIndices } from "./type"
+import type { CellClickHandler, CellIndex, Grid9Values, WinnerCellIndices, PlayableValue } from "./type"
 
 function App() {
-  const values : Grid9Values = [null, null, null, null, '0', null, null, null, 'X'];
+  const values : Grid9Values = [null, null, null, null, null, null, null, null, null];
   const WinnerCellIndices: WinnerCellIndices = null;
 
-  const onCellClick: CellClickHandler = (cellIndex: CellIndex) => {
-    console.log(`Hi ${cellIndex} is clicked`);
-  }
+  const [currentPlayer, setCurrentPlayer] = useState<PlayableValue>("X");
+  const [scoreValues, setScoreValues] = useState<Grid9Values>(values);
+
+  const onCellClick: CellClickHandler = useCallback((cellIndex: CellIndex) => {
+
+    setScoreValues((prevScores: Grid9Values) => {
+      return prevScores.map((item, index) => {
+        if (index === cellIndex)
+          return currentPlayer === "X" ? "X" : "0";
+        else
+          return item;
+      }) as Grid9Values;
+    });
+
+    setCurrentPlayer((prevPlayer) => prevPlayer === "X" ? "0" : "X")
+  }, [currentPlayer])
   
 
   return ( 
     <div>
-      <ScoreBoard currentPlayer='0' gameStatus='Running' winner='X'></ScoreBoard>
-      <TicTacToeGrid grid9Values={values} handleCellClick={onCellClick} winnerIndex={WinnerCellIndices}/>
+      <ScoreBoard currentPlayer={currentPlayer} gameStatus='Running' winner='X'></ScoreBoard>
+      <TicTacToeGrid grid9Values={scoreValues} handleCellClick={onCellClick} winnerIndex={WinnerCellIndices}/>
       <button className='resetButton'>Reset</button>
     </div>
 
