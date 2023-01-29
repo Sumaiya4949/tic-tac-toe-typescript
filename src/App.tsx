8a88dece3618a65b364dec9476119f0832e61d0b
Sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2023-01-29 14:09:45 +0100

    Fix mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,11 @@ function App() {
   const [currentPlayer, setCurrentPlayer] = useState<PlayableValue>("X");
   const [scoreValues, setScoreValues] = useState<Grid9Values>(values);
 
-  const onCellClick: CellClickHandler = useCallback((cellIndex: CellIndex) => {
-
-    if (winner === null) {
-    setScoreValues((prevScores: Grid9Values) => {
-      return prevScores.map((item, index) => {
-        if (index === cellIndex)
-          return currentPlayer === "X" ? "X" : "0";
-        else
-          return item;
-      }) as Grid9Values;
-    });
-
-    setCurrentPlayer((prevPlayer) => prevPlayer === "X" ? "0" : "X")
-  }
-}, [currentPlayer])
-
   const winner = useMemo(() => {
     let i = 0;
     while (i < 7) {
       if ((scoreValues[i] === scoreValues[i+1]) && (scoreValues[i] === scoreValues[i+2]) && scoreValues[i] !== null) {
-        return currentPlayer === "0" ? "X" : "0";
+        return scoreValues[i];
       }
       i = i + 3
     }
@@ -39,22 +23,38 @@ function App() {
     let j = 0;
     while(j < 3) {
       if ((scoreValues[j] === scoreValues[j+3]) && (scoreValues[j] === scoreValues[j+6]) && (scoreValues[j] !== null)){
-        return currentPlayer === "0" ? "X" : "0";
+        return scoreValues[j];
       }
       j = j + 1;
     }
 
     let k = 0;
     if ((scoreValues[k] === scoreValues[k+4]) && (scoreValues[k] === scoreValues[k+8]) && (scoreValues[k] !== null)) {
-      return currentPlayer === "0" ? "X" : "0";
+      return scoreValues[k];
     }
     else if ((scoreValues[k+2] === scoreValues[k+4]) && (scoreValues[k+4] === scoreValues[k+6]) && (scoreValues[k+2] !== null)) {
-      return currentPlayer === "0" ? "X" : "0";
+      return scoreValues[k+2];
     }
     else {
       return null;
     }
-  }, [currentPlayer, scoreValues])
+  }, [scoreValues])
+
+  const onCellClick: CellClickHandler = useCallback((cellIndex: CellIndex) => {
+
+    if (winner === null) {
+      setScoreValues((prevScores: Grid9Values) => {
+        return prevScores.map((item, index) => {
+          if (index === cellIndex)
+            return currentPlayer === "X" ? "X" : "0";
+          else
+            return item;
+        }) as Grid9Values;
+      });
+
+      setCurrentPlayer((prevPlayer) => prevPlayer === "X" ? "0" : "X")
+  }
+}, [currentPlayer, winner])
 
   return ( 
     <div>
